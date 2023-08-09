667d97eb275248cb8001a4673f81abeccedbffd4
laurenzcodes <laurenz1337@gmail.com>
2023-08-09 19:55:36 +0200

    feat(ui): implement ui design + logic for starting game & going back to menu

--- src/scripts/ui-script.js
+++ src/scripts/ui-script.js
@@ -1,29 +1,59 @@
 const uiScript = () => {
   const newGameMenu = document.querySelector(".new-game .menu-options");
+  const gameMenu = document.querySelector(".rps .rps-menu");
 
   if (newGameMenu) {
     newGameMenu.addEventListener("click", (event) => {
       const target = event.target;
 
       if (target.tagName.toLowerCase() === "li") {
-        // Retrieve the action from the data-action attribute
         const action = target.getAttribute("data-action");
 
-        if (action === "pvc") {
-          startGame("pvc");
-        } else if (action === "cvc") {
-          startGame("cvc");
+        if (action === "pvc" || action === "cvc") {
+          startGame(action);
+        }
+      }
+    });
+  }
+
+  if (gameMenu) {
+    gameMenu.addEventListener("click", (event) => {
+      const target = event.target;
+
+      if (target.tagName.toLowerCase() === "li") {
+        const action = target.getAttribute("data-action");
+
+        if (action === "menu") {
+          // Hide the game and show the main menu
+          document.querySelector(".main-menu").style.display = "flex";
+          document.querySelector(".game").style.display = "none";
+        } else if (action === "playAgain") {
+          // Reinitiate the game logic for playing again (placeholder)
         }
       }
     });
   }
 };
 
-// Function to start the game
 const startGame = (mode) => {
-  // Logic to start the game based on the selected mode (pvc or cvc)
-  // You can manipulate the DOM here to change the UI as needed
-  console.log(mode);
+  // Hide the main menu and show the game view
+  document.querySelector(".main-menu").style.display = "none";
+  document.querySelector(".game").style.display = "block";
+
+  // Set game type
+  document.querySelector(".rps .game-type").innerText =
+    mode === "pvc" ? "Player vs Computer" : "Computer vs Computer";
+
+  if (mode === "pvc") {
+    document.querySelector(".rps .hand-picker").style.display = "block";
+    document.querySelector(".rps .rps-gameplay").style.display = "none";
+  } else {
+    document.querySelector(".rps .hand-picker").style.display = "none";
+    document.querySelector(".rps .rps-gameplay").style.display = "grid";
+  }
+  // Show or hide the hand-picker based on the selected mode
+  document.querySelector(".rps .hand-picker").style.display =
+    mode === "pvc" ? "block" : "none";
 };
 
 export default uiScript;
